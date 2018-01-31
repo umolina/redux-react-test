a35575cf990c818e10298a5c58ae15ededa67dc9
Unai Molina <unai.molina.gordillo@gmail.com>
2018-01-31 14:51:34 +0000

    refactoring and cleaning form example

--- src/SyncValidationForm.js
+++ src/SyncValidationForm.js
@@ -1,18 +1,26 @@
 import React from 'react'
-import PropTypes from 'prop-types';
-import {Field, reduxForm, SubmissionError, getFormValues} from 'redux-form'
+import {Field, reduxForm, SubmissionError} from 'redux-form'
 import styled from "styled-components";
 import {Flex, Box} from 'grid-styled'
 import {BrandButton} from './buttons'
-import {TextInput, FormLabel} from './inputs'
-import IntlTelInput from 'react-intl-tel-input';
-import '../node_modules/react-intl-tel-input/dist/libphonenumber.js';
-import '../node_modules/react-intl-tel-input/dist/main.css';
-import './inputs/phone.css';
+import {TextInput, PhoneInput} from './inputs'
 
-import Phone, {isValidPhoneNumber} from 'react-phone-number-input';
-import 'react-phone-number-input/rrui.css'
-import 'react-phone-number-input/style.css'
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+function submit(values) {
+    return sleep(1000) // simulate server latency
+        .then(() => {
+            if (!['Unai', 'paul', 'george', 'ringo'].includes(values.first_name)) {
+                throw new SubmissionError({username: 'User does not exist', _error: 'Login failed!'})
+            } else if (values.password !== '123456') {
+                throw new SubmissionError({password: 'Wrong password', _error: 'Login failed!'})
+            } else {
+                window.alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`)
+            }
+        })
+}
+
+
 
 const validate = values => {
     const errors = {}
@@ -29,210 +37,13 @@ const validate = values => {
         errors.email = 'Invalid email address'
     }
 
-    if (false === values.tel) {
+    if (values.tel && (false === values.tel[0])) {
         errors.tel = 'Valid number required'
     }
 
-    console.log(values)
+    console.log(values.tel)
 
     return errors
-}
-
-let textField = ({input, label, type, autoComplete, meta: {touched, error, warning}, className}) => (
-    <div>
-        <label>{label}</label>
-        <div>
-            <input {...input} placeholder={label} type={type} className={className} autoComplete={autoComplete}/>
-            {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
-        </div>
-    </div>
-)
-
-textField = styled(textField)`
-    background-color: green;
-`
-
-let emailField = ({input, label, type, meta: {touched, error, warning}, className}) => (
-    <div>
-        <label>{label}</label>
-        <div>
-            <input {...input} placeholder={label} type={type} className={className}/>
-            {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
-        </div>
-    </div>
-)
-
-emailField = styled(emailField)`
-    background-color: blue;
-`
-
-const loadJSONP = (url, callback) => {
-    const ref = window.document.getElementsByTagName('script')[0];
-    const script = window.document.createElement('script');
-    script.src = `${url + (url.indexOf('?') + 1 ? '&' : '?')}callback=${callback}`;
-    ref.parentNode.insertBefore(script, ref);
-    script.onload = () => {
-        script.remove();
-    };
-};
-
-const lookup = (callback) => {
-    loadJSONP('http://ipinfo.io', 'sendBack');
-    window.sendBack = (resp) => {
-        const countryCode = (resp && resp.country) ? resp.country : '';
-        callback(countryCode);
-    }
-};
-
-const phoneHandler = (status, value, countryData, number, id) => {
-    console.log(status, value, countryData, number, id);
-};
-
-
-let renderPhoneNumber = ({input: {value, onChange}, label, meta: {touched, error, warning}, className}) => (
-    <div className={className}>
-        <FormLabel>{label} {(error && <span>{error}</span>)}</FormLabel>
-        <IntlTelInput
-            defaultCountry={'auto'}
-            geoIpLookup={lookup}
-            onPhoneNumberChange={onChange}
-            onPhoneNumberBlur={onChange}
-            css={['intl-tel-input', 'form-control']}
-            utilsScript={'libphonenumber.js'}
-            autoComplete={'tel'}
-            fieldName={'tel'}
-            label={label}
-        />
-    </div>
-)
-
-renderPhoneNumber = styled(renderPhoneNumber)`
-    width: 100%;
-    position:relative;
-`
-
-const PhoneInputT = props => {
-    const {placeholder, input, meta, className, label} = props;
-    const {onBlur, onChange, value} = input;
-    const {error} = meta
-
-    let onChangeX = (...args) => {
-        onChange(args);
-    };
-
-    let onBlurX = (...args) => {
-        onBlur(args);
-    };
-
-    return (
-        <div className={className}>
-            <FormLabel>{label} {(error && <span>{error}</span>)}</FormLabel>
-            <IntlTelInput
-                defaultCountry={'auto'}
-                geoIpLookup={lookup}
-                onPhoneNumberChange={onChangeX}
-                onPhoneNumberBlur={onBlurX}
-                css={['intl-tel-input', 'form-control']}
-                utilsScript={'libphonenumber.js'}
-                autoComplete={'tel'}
-                fieldName={'tel'}
-                label={label}
-            />
-        </div>
-    );
-};
-
-
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
-
-function submit(values) {
-    return sleep(1000) // simulate server latency
-        .then(() => {
-            if (!['Unai', 'paul', 'george', 'ringo'].includes(values.first_name)) {
-                throw new SubmissionError({username: 'User does not exist', _error: 'Login failed!'})
-            } else if (values.password !== '123456') {
-                throw new SubmissionError({password: 'Wrong password', _error: 'Login failed!'})
-            } else {
-                window.alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`)
-            }
-        })
-}
-
-
-const PhoneInput = props => {
-    const {placeholder, input, meta, className} = props;
-    const {onBlur, onChange, value} = input;
-    const {error} = meta
-
-    let dictionary = { ES: 'Spain (España) +34'};
-
-    return (
-        <div>
-            <Phone
-                className={className}
-                country="US"
-                placeholder={placeholder}
-                onBlur={onBlurFn(onBlur)}
-                //onBlur={onBlurWorkaroundFn(onBlur, value)}
-                onChange={onChangeFn(onChange)}
-                value={value}
-                convertToNational
-                indicateInvalid
-                dictionary={dictionary}
-            />
-            {error}
-        </div>
-    );
-};
-
-const StyledPhoneInput = styled(PhoneInput)`
-    font-size: 1rem;
-    color: #333333;
-    width: 100%;
-    padding: 1.7rem 0.7rem 0.3rem 0.7rem;
-    border: 0.1rem solid;
-    border-color: #bbbbbb;
-    position: relative;
-    top: 0;
-    left: 0;
-    z-index: 0;
-    background-color: #fff;
-    -webkit-box-sizing: border-box; /* Safari/Chrome, other WebKit */
-    -moz-box-sizing: border-box;    /* Firefox, other Gecko */
-    box-sizing: border-box;         /* Opera/IE 8+ */
-`
-
-
-let PhoneNumberInput = props => {
-    const {input, label, meta, className} = props;
-    const {touched, error, warning} = meta;
-
-    return (
-        <div className={className}>
-            <StyledPhoneInput {...props} />
-            <FormLabel>{label} {touched && ((error && <span>{error}</span>))}</FormLabel>
-        </div>
-    )
-}
-
-PhoneNumberInput = styled(PhoneNumberInput)`
-    width: 100%;
-    position:relative;
-`
-
-
-function onChangeFn(cb) {
-    return value => {
-        console.log('onChange', value);
-        cb(value);
-    };
-}
-
-function onBlurFn(cb) {
-    return event => {
-        console.log('onBlur', event.target.value);
-        cb(event.target.value);
-    };
 }
 
 const SyncValidationForm = (props) => {
@@ -247,7 +58,7 @@ const SyncValidationForm = (props) => {
                     <Field name="first_name" type="text" component={TextInput} label="First name"/>
                 </Box>
                 <Box width={1 / 2} p={1}>
-                    <Field name="tel" type="tel" component={PhoneInputT} label="Phone number"/>
+                    <Field name="tel" type="tel" component={PhoneInput} label="Phone number"/>
                 </Box>
                 <Box width={1 / 2} p={1}>
                     <Field name="password" type="password" component={TextInput} label="Password"
--- src/inputs/index.js
+++ src/inputs/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import styled from "styled-components";
-import { Field, reduxForm } from 'redux-form'
 import IntlTelInput from 'react-intl-tel-input';
+import '../../node_modules/react-intl-tel-input/dist/libphonenumber.js';
+import '../../node_modules/react-intl-tel-input/dist/main.css';
+import './phone.css';
+
 
 const FormInput = styled.input`
     font-size: 1rem;
@@ -29,7 +32,7 @@ export const FormLabel = styled.label`
     z-index: 1;
 `
 
-let TextInputF = ({ input, label, type, autoComplete, meta: { touched, error, warning }, placeholder, className  }) => {
+let TextInputComponent = ({ input, label, type, autoComplete, meta: { touched, error, warning }, placeholder, className  }) => {
     return (
         <div className={className}>
             <FormInput {...input} placeholder={placeholder} type={type} autoComplete={autoComplete}/>
@@ -38,7 +41,62 @@ let TextInputF = ({ input, label, type, autoComplete, meta: { touched, error, wa
     )
 }
 
-export let TextInput = styled(TextInputF)`
+export let TextInput = styled(TextInputComponent)`
+    width: 100%;
+    position:relative;
+`
+
+
+const loadJSONP = (url, callback) => {
+    const ref = window.document.getElementsByTagName('script')[0];
+    const script = window.document.createElement('script');
+    script.src = `${url + (url.indexOf('?') + 1 ? '&' : '?')}callback=${callback}`;
+    ref.parentNode.insertBefore(script, ref);
+    script.onload = () => {
+        script.remove();
+    };
+};
+
+const lookup = (callback) => {
+    loadJSONP('http://ipinfo.io', 'sendBack');
+    window.sendBack = (resp) => {
+        const countryCode = (resp && resp.country) ? resp.country : '';
+        callback(countryCode);
+    }
+};
+
+let PhoneInputComponent = props => {
+    const {placeholder, input, meta, className, label} = props;
+    const {onBlur, onChange, value} = input;
+    const {error} = meta
+
+    let onChangeX = (...args) => {
+        onChange(args);
+    };
+
+    let onBlurX = (...args) => {
+        onBlur(args);
+    };
+
+    return (
+        <div className={className}>
+            <FormLabel>{label} {(error && <span>{error}</span>)}</FormLabel>
+            <IntlTelInput
+                defaultCountry={'auto'}
+                geoIpLookup={lookup}
+                onPhoneNumberChange={onChangeX}
+                onPhoneNumberBlur={onBlurX}
+                css={['intl-tel-input', 'form-control']}
+                utilsScript={'libphonenumber.js'}
+                autoComplete={'tel'}
+                fieldName={'tel'}
+                label={label}
+            />
+        </div>
+    );
+};
+
+export let PhoneInput = styled(PhoneInputComponent)`
     width: 100%;
     position:relative;
 `
